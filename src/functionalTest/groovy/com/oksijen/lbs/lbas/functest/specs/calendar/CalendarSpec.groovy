e111765b279f2fcb6fd6f3188207d9a939f0b33a
ahmet.basak <ahmet.basak@gmlahbasak.telsim.local>
2014-06-12 11:39:13 +0300

    calender added

---
+++ src/functionalTest/groovy/com/oksijen/lbs/lbas/functest/specs/calendar/CalendarSpec.groovy
@@ -0,0 +1,50 @@
+package com.oksijen.lbs.lbas.functest.specs.calendar
+
+import geb.spock.GebSpec
+
+import spock.lang.*
+import static org.hamcrest.Matchers.*
+import static spock.util.matcher.HamcrestSupport.*
+
+import com.oksijen.lbs.lbas.functest.specs.LocateSpec
+
+import com.oksijen.lbs.lbas.functest.pages.LoginPage
+import com.oksijen.lbs.lbas.functest.pages.WelcomePage
+import com.oksijen.lbs.lbas.functest.pages.calendar.*
+
+
+/**
+ * 
+ */
+
+class CalendarSpec extends LocateSpec {
+	
+	def "Month view is displayed"(){
+		given: "We are at the WelcomePage"
+		at WelcomePage
+		calendarMenu.click()
+						
+		when: "Calendar page is displayed"
+		waitFor {at CalendarHomePage}
+				
+		then: "Calendar page should render"
+		expect currentDate.displayed, is(true)
+		
+	}
+	
+	def "Today is highlighted"(){
+		given: "We are at the WelcomePage"
+		at WelcomePage
+		calendarMenu.click()
+						
+		when: "Calendar page is displayed"
+		waitFor {at CalendarHomePage}
+				
+		then: "Calendar page should render"
+		expect highlightToday.displayed, is(true)
+		
+	}
+	
+}
+
+
