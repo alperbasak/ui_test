14b973ba7c982f396b3599f4e252fe301eddce0a
cturkdogan <cturkdogan@vtekl1cturkdogan.telsim.local>
2014-06-12 13:57:37 +0300

    calendarhome

---
+++ src/functionalTest/groovy/com/oksijen/lbs/lbas/functest/pages/calendar/CalendarHomePage.groovy
@@ -0,0 +1,20 @@
+package com.oksijen.lbs.lbas.functest.pages.calendar
+
+import com.oksijen.lbs.lbas.functest.pages.WelcomePage
+
+/**
+ * Created by cpekyaman on 3/25/2014.
+ */
+class CalendarHomePage extends WelcomePage {
+    static url = "welcome"
+    static at = { mainDiv.displayed == true }
+
+    static content = {
+        mainDiv(required: true) 	{ $("div#calendar") }
+        leftPanel(required: true) 	{ $("div.left") }
+        rightPanel(required: true) 	{ $("div.right") }
+		calendarMenu(required: false)  { $("a#btn_calendar")}
+		currentDate(required: false) { $("div.fc-day-number")}
+		highlightToday(required: false) { $("td.fc-thu.fc-widget-content.fc-day17.fc-state-highlight.fc-today")}
+    }
+}
